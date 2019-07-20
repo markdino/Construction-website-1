245ec5dfd262ff84e3d59fac56eb1c8a7e9aee3a
Mark Dino <markdinopelonia447@gmail.com>
2019-07-20 13:02:38 +0800

    feat: Add Hero component

---
+++ src/components/hero.js
@@ -0,0 +1,33 @@
+import React from "react"
+import { Container } from "reactstrap"
+import { Button } from "reactstrap"
+import { Link } from "gatsby"
+import "./hero.scss"
+
+const Hero = ({ title, description }) => {
+  return (
+    <div className="hero">
+      <section className="hero-wrapper">
+        <Container>
+          <main className="hero-content">
+            <h1 className="title">{title}</h1>
+            <hr className="h-separator" />
+            <p className="hero-description">{description}</p>
+            <section className="hero-buttons">
+              <Button color="success" href="#services">
+                Read More
+              </Button>
+              <Link to="/projects">
+                <Button outline color="success">
+                  Projects
+                </Button>
+              </Link>
+            </section>
+          </main>
+        </Container>
+      </section>
+    </div>
+  )
+}
+
+export default Hero
