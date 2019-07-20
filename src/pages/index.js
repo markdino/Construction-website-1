7f0f2847300e4a5e7eceb7507ebd86660e3bd690
Mark Dino <markdinopelonia447@gmail.com>
2019-07-20 13:38:25 +0800

    fix: Remove unused code and insert Layout and Hero components

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,33 @@
 import React from "react"
-import { Link } from "gatsby"
+import { useStaticQuery, graphql } from "gatsby"
+// import { Link } from "gatsby"
+import "bootstrap/dist/css/bootstrap.min.css"
 
+import Hero from "../components/hero"
 import Layout from "../components/layout"
-import Image from "../components/image"
-import SEO from "../components/seo"
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
+const IndexPage = () => {
+  const data = useStaticQuery(graphql`
+    query SiteTitleQuery {
+      site {
+        siteMetadata {
+          title
+          fullTitle
+          description
+        }
+      }
+    }
+  `)
+
+  const { fullTitle, description } = data.site.siteMetadata
+
+  return (
+    <div>
+      <Layout data={data.site.siteMetadata}>
+        <Hero title={fullTitle} description={description} />
+      </Layout>
     </div>
-    <Link to="/page-2/">Go to page 2</Link>
-  </Layout>
-)
+  )
+}
 
 export default IndexPage
