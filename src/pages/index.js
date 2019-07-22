155e0be88c06408d9bbfb5ba1fed911c467d702e
Mark Dino <markdinopelonia447@gmail.com>
2019-07-22 18:14:52 +0800

    core: transfer used color to theme

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,6 +3,10 @@ import { useStaticQuery, graphql } from "gatsby"
 // import { Link } from "gatsby"
 import "bootstrap/dist/css/bootstrap.min.css"
 
+// ---------- Themes colors ----------
+import "../themes/green.scss"
+import "../themes/orange.scss"
+
 import Hero from "../components/hero"
 import Layout from "../components/layout"
 import Services from "../components/services"
