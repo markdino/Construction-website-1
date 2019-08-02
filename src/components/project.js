d8f6d504173ec1ed55dc2a6990d92586b82da11a
Mark Dino <markdinopelonia447@gmail.com>
2019-08-02 10:14:06 +0800

    fix: Gallery categories made dynamic and clickable

--- src/components/project.js
+++ src/components/project.js
@@ -2,17 +2,29 @@ import React, { Component } from "react"
 import { Container, Badge, Row, Col } from "reactstrap"
 import "./project.scss"
 
+const projCategories = ["all", "construction", "remodeling", "interior design"]
 class Project extends Component {
+  state = {
+    category: "all",
+  }
   render() {
+    const { category } = this.state
     return (
       <div className="project section-lg" id="project">
         <Container>
           <h2 className="header-title center">Our Project</h2>
           <section className="gallery-cat center">
-            <Badge color="warning">all</Badge>
-            <Badge color="light">construction</Badge>
-            <Badge color="light">remodeling</Badge>
-            <Badge color="light">interior design</Badge>
+            {projCategories.map((projCategory, index) => {
+              return (
+                <Badge
+                  key={index}
+                  color={projCategory === category ? "warning" : "light"}
+                  onClick={() => this.setState({ category: projCategory })}
+                >
+                  {projCategory}
+                </Badge>
+              )
+            })}
           </section>
           <section className="gallery">
             <Row>
