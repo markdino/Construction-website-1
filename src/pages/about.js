30e0cf76d3a7e18fd25b6bfb54d5d4c2f1860f4c
Mark Dino <markdinopelonia447@gmail.com>
2019-08-26 18:22:55 +0800

    add about-content paragraphs

--- src/pages/about.js
+++ src/pages/about.js
@@ -19,6 +19,13 @@ const AboutPage = () => {
   `)
 
   const { title, fullTitle } = data.site.siteMetadata
+
+  const aboutContent = [
+    `Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ea molestias incidunt reprehenderit. Aliquam harum laboriosam reprehenderit delectus nostrum iste in ullam perspiciatis! Doloremque quibusdam quo quas deleniti. Libero tempore nobis autem pariatur, cumque sunt dolor consequuntur sit velit! Voluptates ea sequi vitae distinctio voluptatem. Quis, tenetur necessitatibus? Dicta, perferendis provident?`,
+    `Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quidem nisi esse dolorum architecto, id et illo, mollitia consectetur eaque debitis enim ea facere provident aperiam repellat nam natus molestiae expedita?`,
+    `Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis voluptatibus cumque incidunt vero corporis architecto vel voluptas optio nemo, cum repellendus, doloremque, voluptates sit? Sit facilis, minus odio tempora dicta distinctio expedita ipsa alias ipsam fuga, nulla dolor, quos ut molestias quia?`,
+    `Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum ipsa officiis aperiam suscipit quaerat non reprehenderit sapiente hic, odit, praesentium dolorem veritatis. Velit dolorum accusamus debitis blanditiis? Ad officia corporis optio neque quaerat dolore, incidunt, ducimus facilis quo ratione, dicta quos voluptatibus ab consequatur unde debitis quibusdam! Temporibus, dolor pariatur ipsa eaque assumenda nesciunt nam! Ab rem ex iusto aut.`,
+  ]
   return (
     <section id="about">
       <PageHeader title="About Us" />
@@ -30,7 +37,12 @@ const AboutPage = () => {
               src={companyThumbnail}
               alt="Company Thumbnail"
               className="img-thumbnail"
+              align="left"
             />
+            <p className="content-title">{fullTitle}</p>
+            {aboutContent.map(para => (
+              <p>{para}</p>
+            ))}
           </section>
         </main>
       </Container>
