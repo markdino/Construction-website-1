02edae59cf5cf5c27dddb635261b639a7432fbae
Mark Dino <markdinopelonia447@gmail.com>
2019-08-28 11:51:43 +0800

    feat: AboutCard component

---
+++ src/components/aboutCard.js
@@ -0,0 +1,17 @@
+import React from "react"
+import "./aboutCard.scss"
+
+const AboutCard = ({ title, children }) => {
+  return (
+    <article className="about-card">
+      <header className="about-card-header">
+        <h4 className="about-card-title">{title}</h4>
+      </header>
+      <section className="about-card-content">
+        <p>{children}</p>
+      </section>
+    </article>
+  )
+}
+
+export default AboutCard
