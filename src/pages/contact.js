3d591d7410234ba01fee972ea1aa230e34cfc50a
Mark Dino <markdinopelonia447@gmail.com>
2019-08-30 09:35:37 +0800

    add contact description

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -34,6 +34,11 @@ const ContactPage = () => {
       <Container>
         <main className="contact-page section-lg">
           <h2 className="header-title center">Get in Touch</h2>
+          <p className="contact-description center text-gray">
+            We are available by fax, e-mail or by phone. You can also use our
+            quick contact form to ask a question about our services and projects
+            we’re working on.
+          </p>
         </main>
       </Container>
       <iframe
