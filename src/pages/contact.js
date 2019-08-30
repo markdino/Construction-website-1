32ca69c79517beed28a53fdad034bf9b02bb0116
Mark Dino <markdinopelonia447@gmail.com>
2019-08-30 15:22:06 +0800

    import svg files and add social links

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -3,6 +3,16 @@ import { Container, Row, Col, Form, Button, Input } from "reactstrap"
 import { useStaticQuery, graphql } from "gatsby"
 import PageHeader from "../components/pageHeader"
 
+import Smartphone from "../assets/svg/smartphone.svg"
+import Telephone from "../assets/svg/phone.svg"
+import Mail from "../assets/svg/mail.svg"
+import Pin from "../assets/svg/map-pin.svg"
+
+import FacebookSVG from "../assets/svg/facebook.svg"
+import TwitterSVG from "../assets/svg/twitter.svg"
+import InstagramSVG from "../assets/svg/instagram.svg"
+import LinkedinSVG from "../assets/svg/linkedin.svg"
+
 const ContactPage = () => {
   const data = useStaticQuery(graphql`
     query ContactdataQuery {
@@ -86,8 +96,38 @@ const ContactPage = () => {
             </Col>
             <Col lg="4" className="pl-5 pr-5">
               <aside className="contact-info">
-                <section className="get-social">
+                <section className="get-social border-bottom">
                   <p className="text-uppercase font-weight-bold">Get Social</p>
+                  <Row className="mb-4 ml-0">
+                    <a
+                      href={social.facebook}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      <FacebookSVG />
+                    </a>
+                    <a
+                      href={social.twitter}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      <TwitterSVG />
+                    </a>
+                    <a
+                      href={social.instagram}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      <InstagramSVG />
+                    </a>
+                    <a
+                      href={social.linkedin}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      <LinkedinSVG />
+                    </a>
+                  </Row>
                 </section>
               </aside>
             </Col>
