1043abe62bade23bb61d68e56316a0a50734f4a1
Mark Dino <markdinopelonia447@gmail.com>
2019-08-30 09:14:09 +0800

    feat: Contact page

---
+++ src/pages/contact.js
@@ -0,0 +1,43 @@
+import React from "react"
+import { Container, Row, Col } from "reactstrap"
+import { useStaticQuery, graphql } from "gatsby"
+import PageHeader from "../components/pageHeader"
+
+const ContactPage = () => {
+  const data = useStaticQuery(graphql`
+    query ContactdataQuery {
+      site {
+        siteMetadata {
+          title
+          email
+          social {
+            facebook
+            twitter
+            instagram
+            linkedin
+          }
+          contact {
+            mobile
+            telephone
+          }
+          address
+          map
+        }
+      }
+    }
+  `)
+
+  const { title, email, social, contact, address, map } = data.site.siteMetadata
+  return (
+    <section id="contact">
+      <PageHeader title="Contact" />
+      <Container>
+        <main className="contact-page section-lg">
+          <h2 className="header-title center">Get in Touch</h2>
+        </main>
+      </Container>
+    </section>
+  )
+}
+
+export default ContactPage
