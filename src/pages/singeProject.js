22e706e04a1e98fca0d795e220278307487e5e95
Mark Dino <markdinopelonia447@gmail.com>
2019-09-10 21:41:36 +0800

    add title and description

--- src/pages/singeProject.js
+++ src/pages/singeProject.js
@@ -5,10 +5,21 @@ class SingeProject extends Component {
   state = {}
   render() {
     return (
-      <main className="singe-project">
+      <main className="singe-project section-lg">
         <Container>
           <Row>
-            <Col lg="5" className="proj-details"></Col>
+            <Col lg="5" className="proj-details">
+              <h2 className="header-title center">Title</h2>
+              <p className="description text-secondary">
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                Necessitatibus autem quasi sunt consequuntur dolorem! Harum
+                delectus recusandae a? Provident rem ab dolorum soluta
+                laudantium aliquam neque, esse, rerum ipsa, sit officiis aut cum
+                pariatur consequatur accusantium non odit quas error corrupti
+                maiores aspernatur ipsam! Numquam deleniti recusandae optio
+                fugiat qui.
+              </p>
+            </Col>
             <Col lg="7" className="proj-photos"></Col>
           </Row>
         </Container>
