3aa58c88ffa6287e5b6a0b243105f1aec50c7fc7
Mark Dino <markdinopelonia447@gmail.com>
2019-09-15 10:35:04 +0800

    add dynamic value in ALT to all img tag

--- src/components/photoViewer.js
+++ src/components/photoViewer.js
@@ -17,14 +17,17 @@ class PhotoViewer extends Component {
     return (
       <main className="photo-viewer w-100 mr-1 ml-1">
         <section className="img-view img-thumbnail mw-100 h-75">
-          <img src={imgList[this.state.imgIndex]} alt="" />
+          <img
+            src={imgList[this.state.imgIndex]}
+            alt={`imageview` + this.state.imgIndex}
+          />
         </section>
         <section className="img-list w-100 d-flex flex-wrap bg-light p-2">
           {imgList.map((img, index) => {
             return (
               <img
                 src={img}
-                alt=""
+                alt={`image` + index}
                 key={index}
                 onClick={() => this.setState({ imgIndex: index })}
                 className={
