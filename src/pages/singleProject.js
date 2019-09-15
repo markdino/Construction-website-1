f779650635d95959d77e692bbad26c12227135a7
Mark Dino <markdinopelonia447@gmail.com>
2019-09-15 11:05:52 +0800

    import sample images to load for PhotoViewer component

--- src/pages/singleProject.js
+++ src/pages/singleProject.js
@@ -1,9 +1,18 @@
 import React, { Component } from "react"
 import { Row, Col, Container } from "reactstrap"
+import PhotoViewer from "../components/photoViewer"
+
+import img1 from "../images/projects/ialicante-mediterranean-homes-2d4lAQAlbDA-unsplash.jpg"
+import img2 from "../images/projects/jarek-ceborski-jn7uVeCdf6U-unsplash.jpg"
+import img3 from "../images/projects/pixasquare-4ojhpgKpS68-unsplash.jpg"
+import img4 from "../images/projects/etienne-beauregard-riverin-B0aCvAVSX8E-unsplash.jpg"
+import img5 from "../images/projects/jason-briscoe-AQl-J19ocWE-unsplash.jpg"
+import img6 from "../images/projects/lance-anderson-QdAAasrZhdk-unsplash.jpg"
 
 class SingeProject extends Component {
   state = {}
   render() {
+    const imgList = [img1, img2, img3, img4, img5, img6]
     return (
       <main className="singe-project section-lg">
         <Container>
@@ -20,7 +29,9 @@ class SingeProject extends Component {
                 fugiat qui.
               </p>
             </Col>
-            <Col lg="7" className="proj-photos"></Col>
+            <Col lg="7" className="proj-photos">
+              <PhotoViewer list={imgList} />
+            </Col>
           </Row>
         </Container>
       </main>
