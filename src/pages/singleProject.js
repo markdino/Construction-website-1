54835f1a16822a6d8606c42717fa8541ff8d6f9c
Mark Dino <markdinopelonia447@gmail.com>
2019-09-18 14:01:23 +0800

    fix: Class component to Function component

--- src/pages/singleProject.js
+++ src/pages/singleProject.js
@@ -17,67 +17,60 @@ import img4 from "../images/projects/etienne-beauregard-riverin-B0aCvAVSX8E-unsp
 import img5 from "../images/projects/jason-briscoe-AQl-J19ocWE-unsplash.jpg"
 import img6 from "../images/projects/lance-anderson-QdAAasrZhdk-unsplash.jpg"
 
-class SingeProject extends Component {
-  state = {}
-  render() {
-    const imgList = [img1, img2, img3, img4, img5, img6]
-    return (
-      <main className="singe-project section-lg">
-        <Container>
-          <Row>
-            <Col lg="5" className="proj-details">
-              <h2 className="header-title center">Title</h2>
-              <p className="description text-secondary">
-                Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-                Necessitatibus autem quasi sunt consequuntur dolorem! Harum
-                delectus recusandae a? Provident rem ab dolorum soluta
-                laudantium aliquam neque, esse, rerum ipsa, sit officiis aut cum
-                pariatur consequatur accusantium non odit quas error corrupti
-                maiores aspernatur ipsam! Numquam deleniti recusandae optio
-                fugiat qui.
-              </p>
-              <section className="details mt-5 mb-5">
-                <List1
-                  svg={<User />}
-                  title="Owner"
-                  value="Ms. Marissa Pelonia"
-                />
-                <hr />
-                <List1
-                  svg={<Pin />}
-                  title="Location"
-                  value="Block2, Lot1, Sierra Madre Street, Bermuda Subdivision, Las Pinas City, Philippines"
-                />
-                <hr />
-                <List1
-                  svg={<Building />}
-                  title="Scope of Works"
-                  value="Construction"
-                />
-                <hr />
-                <List1
-                  svg={<Hardhat />}
-                  title="Project Engineer"
-                  value="Engr. Zaldy Omar Pelonia"
-                />
-                <hr />
-                <List1 svg={<Bill />} title="Amount" value="Php 4,580,240.23" />
-                <hr />
-                <List1
-                  svg={<Calendar />}
-                  title="Duration Date"
-                  value="Feb. 24, 2018 - March 05, 2019"
-                />
-              </section>
-            </Col>
-            <Col lg="7" className="proj-photos">
-              <PhotoViewer list={imgList} />
-            </Col>
-          </Row>
-        </Container>
-      </main>
-    )
-  }
+const imgList = [img1, img2, img3, img4, img5, img6]
+
+const SingeProject = () => {
+  return (
+    <main className="singe-project section-lg">
+      <Container>
+        <Row>
+          <Col lg="5" className="proj-details">
+            <h2 className="header-title center">Title</h2>
+            <p className="description text-secondary">
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+              Necessitatibus autem quasi sunt consequuntur dolorem! Harum
+              delectus recusandae a? Provident rem ab dolorum soluta laudantium
+              aliquam neque, esse, rerum ipsa, sit officiis aut cum pariatur
+              consequatur accusantium non odit quas error corrupti maiores
+              aspernatur ipsam! Numquam deleniti recusandae optio fugiat qui.
+            </p>
+            <section className="details mt-5 mb-5">
+              <List1 svg={<User />} title="Owner" value="Ms. Marissa Pelonia" />
+              <hr />
+              <List1
+                svg={<Pin />}
+                title="Location"
+                value="Block2, Lot1, Sierra Madre Street, Bermuda Subdivision, Las Pinas City, Philippines"
+              />
+              <hr />
+              <List1
+                svg={<Building />}
+                title="Scope of Works"
+                value="Construction"
+              />
+              <hr />
+              <List1
+                svg={<Hardhat />}
+                title="Project Engineer"
+                value="Engr. Zaldy Omar Pelonia"
+              />
+              <hr />
+              <List1 svg={<Bill />} title="Amount" value="Php 4,580,240.23" />
+              <hr />
+              <List1
+                svg={<Calendar />}
+                title="Duration Date"
+                value="Feb. 24, 2018 - March 05, 2019"
+              />
+            </section>
+          </Col>
+          <Col lg="7" className="proj-photos">
+            <PhotoViewer list={imgList} />
+          </Col>
+        </Row>
+      </Container>
+    </main>
+  )
 }
 
 export default SingeProject
