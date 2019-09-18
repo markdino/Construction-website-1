14ef2bef20c6e4e7a36c472d7031f4c8d6023a10
Mark Dino <markdinopelonia447@gmail.com>
2019-09-18 14:09:50 +0800

    add Link

--- src/components/projCard.js
+++ src/components/projCard.js
@@ -1,16 +1,19 @@
 import React from "react"
+import { Link } from "gatsby"
 
 const ProjCard = ({ proj }) => {
   const { img, title, description } = proj
   return (
     <div className="proj-card">
-      <img src={img.src} alt={img.orig} title={img.author} />
-      <section className="wrap-dark">
-        <main className="proj-content center">
-          <h4>{title}</h4>
-          <p>{description}</p>
-        </main>
-      </section>
+      <Link to="singleProject" state={{ project: proj }}>
+        <img src={img.src} alt={img.orig} title={img.author} />
+        <section className="wrap-dark">
+          <main className="proj-content center">
+            <h4>{title}</h4>
+            <p>{description}</p>
+          </main>
+        </section>
+      </Link>
     </div>
   )
 }
