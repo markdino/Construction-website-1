ca630aa43d7ba223405bc0881bde84020b832ed6
Mark Dino <markdinopelonia447@gmail.com>
2019-09-18 15:45:04 +0800

    received data pass by ProjCard Link

--- src/pages/singleProject.js
+++ src/pages/singleProject.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react"
+import React from "react"
 import { Row, Col, Container } from "reactstrap"
 import PhotoViewer from "../components/photoViewer"
 import List1 from "../components/list1"
@@ -10,62 +10,54 @@ import Bill from "../assets/svg/solid/money-bill-wave.svg"
 import Building from "../assets/svg/solid/building.svg"
 import Hardhat from "../assets/svg/solid/hard-hat.svg"
 
-import img1 from "../images/projects/ialicante-mediterranean-homes-2d4lAQAlbDA-unsplash.jpg"
-import img2 from "../images/projects/jarek-ceborski-jn7uVeCdf6U-unsplash.jpg"
-import img3 from "../images/projects/pixasquare-4ojhpgKpS68-unsplash.jpg"
-import img4 from "../images/projects/etienne-beauregard-riverin-B0aCvAVSX8E-unsplash.jpg"
-import img5 from "../images/projects/jason-briscoe-AQl-J19ocWE-unsplash.jpg"
-import img6 from "../images/projects/lance-anderson-QdAAasrZhdk-unsplash.jpg"
+const SingeProject = ({ location }) => {
+  const {
+    title,
+    category,
+    description,
+    img,
+    engineer,
+    amount,
+    duration,
+    owner,
+    address,
+  } = location.state.project
 
-const imgList = [img1, img2, img3, img4, img5, img6]
-
-const SingeProject = () => {
   return (
     <main className="singe-project section-lg">
       <Container>
         <Row>
           <Col lg="5" className="proj-details">
-            <h2 className="header-title center">Title</h2>
-            <p className="description text-secondary">
-              Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-              Necessitatibus autem quasi sunt consequuntur dolorem! Harum
-              delectus recusandae a? Provident rem ab dolorum soluta laudantium
-              aliquam neque, esse, rerum ipsa, sit officiis aut cum pariatur
-              consequatur accusantium non odit quas error corrupti maiores
-              aspernatur ipsam! Numquam deleniti recusandae optio fugiat qui.
-            </p>
+            <h2 className="header-title center">{title}</h2>
+            <p className="description text-secondary">{description}</p>
             <section className="details mt-5 mb-5">
-              <List1 svg={<User />} title="Owner" value="Ms. Marissa Pelonia" />
+              <List1 svg={<User />} title="Owner" value={owner} />
               <hr />
-              <List1
-                svg={<Pin />}
-                title="Location"
-                value="Block2, Lot1, Sierra Madre Street, Bermuda Subdivision, Las Pinas City, Philippines"
-              />
+              <List1 svg={<Pin />} title="Location" value={address} />
               <hr />
               <List1
                 svg={<Building />}
                 title="Scope of Works"
-                value="Construction"
+                value={category.map(cat => cat)}
               />
               <hr />
               <List1
                 svg={<Hardhat />}
                 title="Project Engineer"
-                value="Engr. Zaldy Omar Pelonia"
+                value={engineer}
               />
               <hr />
-              <List1 svg={<Bill />} title="Amount" value="Php 4,580,240.23" />
+              <List1 svg={<Bill />} title="Amount" value={amount} />
               <hr />
               <List1
                 svg={<Calendar />}
                 title="Duration Date"
-                value="Feb. 24, 2018 - March 05, 2019"
+                value={duration}
               />
             </section>
           </Col>
           <Col lg="7" className="proj-photos">
-            <PhotoViewer list={imgList} />
+            <PhotoViewer list={img} />
           </Col>
         </Row>
       </Container>
