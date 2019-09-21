68fe08a8af535b99f691148e85eba0750d43dc05
Mark Dino <markdinopelonia447@gmail.com>
2019-09-21 13:58:42 +0800

    change link base by slug

--- src/components/projCard.js
+++ src/components/projCard.js
@@ -4,10 +4,10 @@ import { Link } from "gatsby"
 import "./projCard.scss"
 
 const ProjCard = ({ proj }) => {
-  const { img, title, description } = proj
+  const { img, title, description, slug } = proj
   return (
     <div className="proj-card">
-      <Link to="singleProject/#single-project" state={{ project: proj }}>
+      <Link to={`projects/${slug}/#single-project`}>
         <img src={img.src[0]} alt={img.orig} title={img.author} />
         <section className="wrap-dark">
           <main className="proj-content center">
