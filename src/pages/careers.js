593ef3ff09225fdf1bc5ee28efe7a434734c6f4a
Mark Dino <markdinopelonia447@gmail.com>
2019-10-01 15:52:27 +0800

    use Component for the table of oppurtunities

--- src/pages/careers.js
+++ src/pages/careers.js
@@ -1,17 +1,19 @@
 import React from "react"
-import { Container, Row, Col, Button, Table } from "reactstrap"
+import { Container, Row, Col, Button } from "reactstrap"
 import { graphql, Link } from "gatsby"
 import "./careers.scss"
 import PageHeader from "../components/pageHeader"
 import List2 from "../components/list2"
+import Table1 from "../components/table1"
 
 import Check from "../assets/svg/solid/check.svg"
 
 import teamThumbnail from "../images/team.jpg"
 
 const CareersPage = ({ data }) => {
-  const opportunities = data.allCareersJson.edges
   const careersInfo = data.site.siteMetadata.careers
+  const tableHeader = ["Job Posting Title", "Location", "Time"]
+  const opportunities = data.allCareersJson.edges
   return (
     <section id="careers">
       <PageHeader title="Careers" />
@@ -51,29 +53,11 @@ const CareersPage = ({ data }) => {
             </Col>
           </Row>
           <Row>
-            <section className="opened-positions mt-5 w-100">
-              <h3>Opened Positions</h3>
-              <Table>
-                <thead>
-                  <tr>
-                    <th>Job Posting Title</th>
-                    <th>Location</th>
-                    <th>Time</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  {opportunities.map(career => {
-                    return (
-                      <tr>
-                        <td>{career.node.positionTitle}</td>
-                        <td>{career.node.location}</td>
-                        <td>{career.node.time}</td>
-                      </tr>
-                    )
-                  })}
-                </tbody>
-              </Table>
-            </section>
+            <Table1
+              title="Opened Position"
+              tHeader={tableHeader}
+              tData={opportunities}
+            />
           </Row>
         </main>
       </Container>
