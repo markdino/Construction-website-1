24bb73f6ea57cc9b4b6a1a8a3e6c9451f81381f9
Mark Dino <markdinopelonia447@gmail.com>
2019-10-02 17:09:31 +0800

    remove unused style

--- src/components/list1.js
+++ src/components/list1.js
@@ -3,10 +3,10 @@ import "./list1.scss"
 
 const List1 = ({ svg, title, value }) => {
   return (
-    <section className="list1 d-flex flex-nowrap align-content-center">
+    <section className="list1 d-flex flex-nowrap align-items-baseline">
       <div className="svg mr-3 d-flex align-items-center">{svg}</div>
-      <p className="list-title mr-3">{`${title} :`} </p>
-      <p className="list-value text-muted"> {value}</p>
+      <div className="list-title mr-3 text-nowrap">{`${title} :`} </div>
+      <div className="list-value text-muted"> {value}</div>
     </section>
   )
 }
