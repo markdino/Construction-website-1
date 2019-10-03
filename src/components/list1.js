6ac5800c770c9df20d48d45794d17f11c12a5001
Mark Dino <markdinopelonia447@gmail.com>
2019-10-03 14:21:59 +0800

    make List1 component customizable

--- src/components/list1.js
+++ src/components/list1.js
@@ -1,11 +1,28 @@
 import React from "react"
 
-const List1 = ({ svg, title, value }) => {
+const List1 = ({
+  alignItems,
+  svg,
+  title,
+  className,
+  alignSVG,
+  titleClass,
+  value,
+  valueColor,
+  valueClass,
+}) => {
   return (
-    <section className="list font-size-120 d-flex flex-nowrap align-items-baseline">
-      <div className="svg mr-3 d-flex align-items-center">{svg}</div>
-      <div className="list-title mr-3 text-nowrap">{`${title} :`} </div>
-      <div className="list-value text-muted"> {value}</div>
+    <section
+      className={`list d-flex flex-nowrap align-items-${alignItems ||
+        `baseline`} ${className}`}
+    >
+      <div className={`svg mr-3 d-flex align-items-${alignSVG || `center`}`}>
+        {svg}
+      </div>
+      <div className={`mr-3 text-nowrap ${titleClass}`}>{`${title} :`} </div>
+      <div className={`text-${valueColor || `muted`} ${valueClass}`}>
+        {value}
+      </div>
     </section>
   )
 }
